c5cb3b80dd0351c8b5505ef90310d1cee09b7e0d
ayxan2007-hlbrtn <aykajah9@gmail.com>
2026-04-11 09:31:06 +0000

    Fix pycodestyle E501: line too long

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 import sys
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]  # Proqramın adını çıxırıq, yalnız arqumentləri saxlayırıq
+    argv = sys.argv[1:]
     count = len(argv)
 
     if count == 0:
