97f9d58bd478813a8ce94fbed10c66424fe1e553
ayxan2007-hlbrtn <aykahaj9@gmail.com>
2026-04-15 19:35:05 +0000

    Task 4: Replace element in a copy of the list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Orijinal siyahının kopyasını yaradırıq
     new_list = my_list.copy()
-    
-    # İndeks düzgündürsə, kopyanın daxilində dəyişiklik edirik
     if 0 <= idx < len(my_list):
         new_list[idx] = element
-        
     return new_list
